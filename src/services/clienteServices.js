dc9d6441d34f6b0292952cbda867b3dae241e152
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2025-07-28 15:53:07 -0500

    feat: :sparkles: Customer Service is created
    This file handles CRUD with MongoDB.

---
+++ src/services/clienteServices.js
@@ -0,0 +1,40 @@
+const { getDB } = require('../config/mongo');
+const Cliente = require('../models/cliente.js');
+
+async function crearCliente(data) {
+    const errores = Cliente.validar(data);
+    if(errores.length) throw new Error (errores.join(', '));
+
+    const db = await getDB();
+    const coleccion = db.coleccion('clientes');
+
+    const existe = await coleccion.findOne({correo: data.correo});
+    if (existe) throw new Error("❌ Cliente con este correo ya existe");
+
+    const nuevoCliente = new Cliente(data);
+    const resultado = await coleccion.insertOne(nuevoCliente);
+    return resultado.insertedId;
+}
+
+async function listarClientes(){
+    const db = await getDB();
+    return db.coleccion('clientes').find({}).toArray();
+}
+
+async function editarCliente(id, nuevosDatos) {
+    const db = await getDb();
+    return db.collection('clientes').updateOne({ _id: id }, { $set: nuevosDatos });
+}
+
+async function eliminarCliente(id) {
+    const db = await getDb();
+    return db.collection('clientes').deleteOne({ _id: id });
+}
+
+
+module.exports = {
+    crearCliente,
+    listarClientes,
+    editarCliente,
+    eliminarCliente,
+};
