8ce4ab2ed988a8e521e106bc145a72cfab9d239b
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2025-07-29 13:43:57 -0500

    feat: :sparkles: The proposal.js model is created
    The proposal.js model is created where the data is received and validation is performed to ensure that it is entered correctly.

---
+++ src/models/propuesta.js
@@ -0,0 +1,24 @@
+class Propuesta {
+    constructor({clienteId, descripcion, precio, plazoDias}){
+        this.clienteId = clienteId;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.plazoDias = plazoDias;
+        this.estado = 'pendiente'; // por defecto
+        this.createdAt = new Date();
+
+    }
+
+    static validar(propuesta){
+        const errores = [ ]
+
+        if (!propuesta.clienteId) errores.push("Cliente asociado obligatorio");
+        if (!propuesta.descripcion || typeof propuesta.descripcion !== 'string') errores.push("Descripción inválida");
+        if (!propuesta.precio || typeof propuesta.precio !== 'number' || propuesta.precio <= 0) errores.push("Precio inválido");
+        if (!propuesta.plazoDias || typeof propuesta.plazoDias !== 'number' || propuesta.plazoDias < 1) errores.push("Plazo inválido");
+
+        return errores;
+    }
+}
+\
+module.export = Propuesta;
